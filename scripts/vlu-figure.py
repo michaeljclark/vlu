0d4ffa102763f18f2817a517d5e7114f90035abc
Michael Clark <michaeljclark@mac.com>
2019-10-27 11:13:35 +1300

    Parameterize byte size in vlu illustration script

--- scripts/vlu-figure.py
+++ scripts/vlu-figure.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python
 
-with open('vlu8-figure.tex', 'w') as fp:
+p = 8
+q = 8
+name = "byte"
+scale = 0.25
+
+with open('vlu-figure.tex', 'w') as fp:
 
     header = """
     \\documentclass{report}
@@ -13,50 +18,51 @@ with open('vlu8-figure.tex', 'w') as fp:
     \\begin{document}
     \\begin{figure}[htp]
     \\centering
-    \\paragraph{VLU8 - Variable Length Unary Integer Coding \\textit{(VLU, bits=8)}}
+    \\paragraph{VLU%d - Variable Length Unary Integer Coding \\textit{(VLU, bits=%d)}}
     \\paragraph{ }
     \\paragraph{ }
-    \\begin{tikzpicture}[fill=blue!20,scale=0.25]
+    \\begin{tikzpicture}[fill=blue!20,scale=%f]
     \\begin{scope}[every node/.style={font=\\scriptsize}]
-    """
+    """ % (p, p, scale)
     fp.write(header)
 
-    for x in range(8, 72, 8):
+    for x in range(p, (p+1)*q, p):
       fp.write("\\fill [black!%d] (%d,-1)   rectangle (%d,-2);\n"
-              % (((x>>3)%2)*10+10, x, x-8))
+              % (((x>>3)%2)*10+10, x, x-p))
     fp.write("\n")
-    for x in range(8, 72, 8):
+    for x in range(p, (p+1)*q, p):
       fp.write("\\draw [black!50] (%d,-1)   rectangle (%d,-2);\n"
-              % (x, x-8))
+              % (x, x-p))
     fp.write("\n")
-    for x in range(0, 64, 8):
-      fp.write("\\node (bit_%d)    at (%5.2f  ,-1.5) {$7$};\n"
-                 % (63 - x, x + 0.5))
+    for x in range(0, p*q, p):
+      fp.write("\\node (bit_%d)    at (%5.2f  ,-1.5) {$%s$};\n"
+                 % (p*q-1, x + 0.5, p-1))
       fp.write("\\node (bit_%d)    at (%5.2f  ,-1.5) {$0$};\n"
-                 % (56 - x, x + 7.5))
+                 % (p*(q-1), x + p - 0.5))
     fp.write("\n")
-    for x in range(0,8,1):
-        fp.write("\\node (byte_%d)     at (%f ,%f) {$byte-%d$};\n"
-               % (x, 4 + (7-x)*8, 0, x + 1 ))
+    for x in range(0,q,1):
+        fp.write("\\node (byte_%d)     at (%f ,%f) {$%s-%d$};\n"
+               % (x, 4 + (q-1-x)*p, 0, name, x + 1 ))
     fp.write("\n")
-    for u in range(0, 8, 1):
+
+    for u in range(0, q, 1):
         y = u
-        fp.write("\\node (a_0)     at (69.0 ,%f) {$%d%sbit$};\n"
-                 % (-2.5 - y, (u+1) * 7, '+' if u == 7 else '-'))
-        for x in range(64, 64 - (u+1)*8, -8):
+        fp.write("\\node (a_0)     at (%f ,%f) {$%d/%d%sbit$};\n"
+                 % (p*q + 5, -2.5 - y, (u+1) * p, (u+1) * (p-1), '+' if u == p-1 else '-'))
+        for x in range(p*q, p*q - (u+1)*p, -p):
             s = ((x>>3)%2)*10+10
             fp.write("\\fill [black!%d] (%d,%f)   rectangle (%d,%f);\n"
-              % (s, x, -2 - y, x-8, -3.0 - y))
+              % (s, x, -2 - y, x-p, -3.0 - y))
             fp.write("\\draw [black!50] (%d,%d)   rectangle (%d,%d);\n"
-              % (   x, -2 - y, x-8, -3.0 - y))
+              % (   x, -2 - y, x-p, -3.0 - y))
         fp.write("\\node (bit_0_%d)    at ( %f, %f ) {$%s$};\n"
-                  % (u,   0.5 + (64-(u+1)), -2.5 - y, 'c' if u == 7 else '0' ));
+                  % (u,   0.5 + (p*q-(u+1)), -2.5 - y, 'c' if u == p-1 else '0' ));
         for x in range(1, u + 1, 1):
             fp.write("\\node (bit_%d_%d)    at ( %f, %f ) {$1$};\n"
-                  % (u,x, 0.5 + (64-x), -2.5 - y ));
-        for x in range(u + 2, u + (u + 1) * 7 + 2, 1):
+                  % (u,x, 0.5 + (p*q-x), -2.5 - y ));
+        for x in range(u + 2, u + (u + 1) * (p-1) + 2, 1):
             fp.write("\\node (bit_%d_%d)    at ( %f, %f ) {$n$};\n"
-                  % (u,x, 0.5 + (64-x), -2.5 - y ));
+                  % (u,x, 0.5 + (p*q-x), -2.5 - y ));
     fp.write("\n")
 
     footer = """
