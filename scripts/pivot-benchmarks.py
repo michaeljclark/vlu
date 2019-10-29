06511011495e30c8b5512be45ece76eb5abb244e
Michael Clark <michaeljclark@mac.com>
2019-10-30 12:34:43 +1300

    Add markdown table support to benchmark pivot table script

--- scripts/pivot-benchmarks.py
+++ scripts/pivot-benchmarks.py
@@ -1,9 +1,14 @@
 #!/usr/bin/python3
 
 import re;
+import argparse
 
 # Benchmark     <dist>-<op> <dist>-<op> <dist>-<op> <dist>-<op>
 # <name>        <rate>      <rate>      <rate>      <rate>
+
+parser = argparse.ArgumentParser(description='Pivot benchmark data')
+parser.add_argument('-m', '--markdown', nargs='?', const=True, default=False, help='markdown format')
+args = parser.parse_args()
 
 rates = {}
 benchs = []
@@ -26,17 +31,31 @@ with open("data/benchmarks.md") as f:
                 benchs.append(bench)
             rates[key] = rate
 
-print("%-20s" % "Benchmark", end = '')
+sep = '|' if args.markdown else ''
+
+if args.markdown:
+    print("|", end = '')
+print("%-28s" % "Benchmark", end = sep)
 for col in cols:
-    print("%-20s" % col, end = '')
+    print("%-20s" % col, end = sep)
 print('')
 
+if args.markdown:
+    print("|:%27s" % ('-' * 27), end = sep)
+    for col in cols:
+        print("%19s:" % ('-' * 19), end = sep)
+    print('')
+
 for bench in benchs:
-    print("%-20s" % bench.replace('_','-'), end = '')
+    name = bench.replace('_','-')
+    if args.markdown:
+        name = name.replace('-', '&#8209;')
+        print("|", end = '')
+    print("%-28s" % name, end = sep)
     for distop in cols:
         key = "%s-%s" % (bench, distop)
         if key in rates:
-            print("%-20s" % rates[key], end = '')
+            print("%-20s" % rates[key], end = sep)
         else:
-            print("%-20s" % "?", end = '')
+            print("%-20s" % "?", end = sep)
     print('')
